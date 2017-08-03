dac0db603e667f19516ab482fd9c90b0dc752a7f
imtiyazgit <imtiyaz.208@gmail.com>
2017-08-03 16:27:23 -0700

    mods

---
+++ src/main/java/com/imtiyaz/number/NumEvenOrOdd.java
@@ -0,0 +1,16 @@
+package com.imtiyaz.number;
+
+/**
+ * Created by imtiyaz on 8/3/17.
+ */
+public class NumEvenOrOdd {
+    public static void main(String[] args) {
+        int num = 24;
+
+        if(num%2==0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
