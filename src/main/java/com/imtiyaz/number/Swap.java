5be21b879f356390ee5fbfd977a11d36c6633b38
imtiyazgit <imtiyaz.208@gmail.com>
2017-08-03 17:08:37 -0700

    mods

--- src/main/java/com/imtiyaz/number/Swap.java
+++ src/main/java/com/imtiyaz/number/Swap.java
@@ -4,7 +4,22 @@ package com.imtiyaz.number;
  * Created by imtiyaz on 8/3/17.
  */
 public class Swap {
+
     public static void main(String[] args) {
+        int a=3;
+        int b=5;
+
+        int temp;
+        temp=b;
+        b=a;
+        a=temp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    public static void main2(String[] args) {
+        // without temp variable
         int a=3;
         int b=10;
 
