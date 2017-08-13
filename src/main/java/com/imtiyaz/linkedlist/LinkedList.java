16cd70959290631fd37e8997df5c6f1c25e0d02d
imtiyazgit <imtiyaz.208@gmail.com>
2017-08-13 13:12:01 -0700

    mods

--- src/main/java/com/imtiyaz/linkedlist/LinkedList.java
+++ src/main/java/com/imtiyaz/linkedlist/LinkedList.java
@@ -9,17 +9,55 @@ public class LinkedList {
 
     public void insertAtHead(int data) {
         Node node = new Node(data);
-        node.setNextNode(head);
-        head = node;
+        node.setNextNode(this.head);
+        this.head = node;
     }
 
+    public int length() {
+        Node current = head;
+        int length=0;
+        while(current!=null) {
+            length++;
+            current = current.getNextNode();
+        }
+        return length;
+    }
+
+    public int findMiddle() {
+        Node current = head;
+        int j=0;
+        for(int i=0; current != null; i++) {
+            if(i%2==0) {
+                j++;
+            }
+            current = current.getNextNode();
+        }
+        return j;
+    }
+
+
+    public void deleteHeadNode() {
+        head = head.getNextNode();
+    }
+
+    public Node find(int data) {
+        Node current = head;
+        while(current != null) {
+            if(current.getData() ==  data) {
+                return current;
+            }
+            current = current.getNextNode();
+        }
+        return null;
+    }
 
     @Override
     public String toString() {
         String result = "{";
         Node current = head;
-        while(current.getNextNode()!= null) {
+        while(current != null) {
             result += current + ",";
+            current = current.getNextNode();
         }
 
         result += "}";
--- src/main/java/com/imtiyaz/linkedlist/LinkedListDemo.java
+++ src/main/java/com/imtiyaz/linkedlist/LinkedListDemo.java
@@ -6,10 +6,28 @@ package com.imtiyaz.linkedlist;
 public class LinkedListDemo {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList();
+        linkedList.insertAtHead(9);
+        linkedList.insertAtHead(8);
+        linkedList.insertAtHead(7);
+        linkedList.insertAtHead(6);
+        linkedList.insertAtHead(5);
+        linkedList.insertAtHead(4);
         linkedList.insertAtHead(3);
         linkedList.insertAtHead(2);
         linkedList.insertAtHead(1);
 
         System.out.println(linkedList);
+
+        System.out.println("Found"+linkedList.find(2));
+
+        linkedList.deleteHeadNode();
+
+        System.out.println(linkedList);
+
+        System.out.println(linkedList.length());
+
+        System.out.println(linkedList.findMiddle());
+
+
     }
 }
