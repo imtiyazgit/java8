caeb648bbc23a19a929091fc351cd2baff8b526e
imtiyazgit <imtiyaz.208@gmail.com>
2017-08-16 16:55:42 -0700

    mods

--- src/main/java/com/imtiyaz/linkedlist2/LinkedList.java
+++ src/main/java/com/imtiyaz/linkedlist2/LinkedList.java
@@ -10,6 +10,20 @@ public class LinkedList<T> {
         head = newNode;
     }
 
+    public void deleteHead() {
+        head = head.next;
+    }
+
+    public int size() {
+        int length=0;
+        Node current = head;
+        while(current!=null) {
+            length++;
+            current = current.next;
+        }
+        return length;
+    }
+
 
     private static class Node<T> {
         private T data;
@@ -39,7 +53,17 @@ public class LinkedList<T> {
         LinkedList<Integer> linkedList = new LinkedList<Integer>();
         linkedList.insertAtHead(2);
         linkedList.insertAtHead(3);
-        linkedList.insertAtHead(1);
+        linkedList.insertAtHead(4);
+        linkedList.insertAtHead(9);
+        linkedList.insertAtHead(8);
+        linkedList.insertAtHead(10);
+        linkedList.insertAtHead(5);
+        linkedList.insertAtHead(7);
+
+        System.out.println(linkedList +" and size is "+linkedList.size());
+
+        System.out.println("Delete head");
+        linkedList.deleteHead();
         System.out.println(linkedList);
     }
 }
